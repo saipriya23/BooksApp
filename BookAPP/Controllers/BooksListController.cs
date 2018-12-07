79af2c9a7dabc98b9ceed95610a2082144c9207c
saipriya23 <saipriyareddy2307@gmail.com>
2018-12-07 18:24:05 +0530

    books

---
+++ BookAPP/Controllers/BooksListController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPP.Controllers
+{
+    public class BooksListController : Controller
+    {
+        private List<BooksDetails> Books = new List<BooksDetails>()
+        {
+           new BooksDetails{Name="Book1",Quantity=2},
+           new BooksDetails{Name="Book2",Quantity=4},
+           new BooksDetails{Name="Book3",Quantity=5},
+           new BooksDetails{Name="Book4",Quantity=6},
+
+        };
+        public IActionResult Index()
+        {
+            return View(Books);
+        }
+        public IActionResult BorrowBooks()
+        {
+            return View();
+        }
+        public IActionResult AddBooks()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult BorrowBooks(BooksDetails Book)
+        {
+            foreach(var item in Books)
+            {
+                if(item.Name==Book.Name)
+                {
+                    item.Quantity = item.Quantity - Book.Quantity;
+                }
+              //foreach (var quant in Books)
+                  //  if (item.Quantity == 0)
+                Books.Remove(Books.Single(x => x.Quantity == 0));
+                return View("index", Books);
+
+                //if(item.Quantity==0)
+                //{
+                //    Books.Remove(item.Name);
+                //}
+            }
+            return View("index", Books);
+
+            
+        }
+        [HttpPost]
+        public IActionResult AddBooks(BooksDetails BookName)
+        {
+            Books.Add(new BooksDetails() {Name=BookName.Name,Quantity=BookName.Quantity });
+            return View("index",Books);
+        }
+        
+    }
+}
---
+++ BookAPP/Models/BooksDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPP.Models
+{
+    public class BooksDetails
+    {
+        public string Name { get; set; }
+        public  int Quantity { get; set; }
+
+    }
+}
